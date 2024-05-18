873b8aa522983383912931e1f17512fe70d56e0b
brymmobaggins <ibro2tablet21@yahoo.com>
2024-05-18 03:41:36 +0100

    add pop up modal

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,22 @@
 
 
+const closeButton = document.querySelector('#close-button')
+const openButton = document.querySelector('#open-button')
+const modal = document.querySelector('#modal')
+console.log(modal)
 
-const button = document.querySelector('button')
-console.log(button)
+function openModal() {
+    modal.style.display = "block"
+}
+function closeModal() {
+    modal.style.display = "none"
+}
+closeButton.addEventListener("click", function () {
+    closeModal()
+})
+openButton.addEventListener("click", function () {
+    openModal()
+
+})
+
+// alert("")
