d16e7d52acba469067e5aaaf90fc197f17482020
brymmobaggins <ibro2tablet21@yahoo.com>
2024-05-20 15:57:42 +0100

    add js code to get the value

--- dist/app.js
+++ dist/app.js
@@ -1,9 +1,47 @@
 
-
+const form = document.querySelector("form")
 const closeButton = document.querySelector('#close-button')
 const openButton = document.querySelector('#open-button')
 const modal = document.querySelector('#modal')
-console.log(modal)
+// console.log(modal)
+
+const itemList = document.querySelector("#item-list")
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault()
+    getvalues()
+})
+
+function getvalues() {
+    let itemName = document.getElementById("item-name").value
+    let itemCategory = document.getElementById("item-category").value
+    let itemAmount = document.getElementById("item-amount").value
+    let expenseDate = document.getElementById("expense-date").value
+    console.log(itemName)
+
+    if (itemName && itemCategory && itemAmount && expenseDate) {
+
+        itemList.innerHTML =
+        `<tr class="*:p-2 *:font-sans">
+
+           <td>${itemName}</td>
+           <td>${itemCategory}</td> 
+           <td>${itemAmount}</td>
+           <td>${expenseDate}</td>
+
+        </tr>
+        `
+
+        closeModal()
+
+    }else{
+        
+    }
+}
+
+
+
+
 
 function openModal() {
     modal.style.display = "block"
@@ -19,4 +57,4 @@ openButton.addEventListener("click", function () {
 
 })
 
-// alert("")
+// function 
