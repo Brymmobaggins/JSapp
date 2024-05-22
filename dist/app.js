ae55eaef90981bdd2d21bd0f86d07164475e98a4
brymmobaggins <ibro2tablet21@yahoo.com>
2024-05-22 16:08:24 +0100

    add getting the values and append to the DOM

--- dist/app.js
+++ dist/app.js
@@ -13,32 +13,54 @@ form.addEventListener("submit", function (e) {
 })
 
 function getvalues() {
-    let itemName = document.getElementById("item-name").value
-    let itemCategory = document.getElementById("item-category").value
-    let itemAmount = document.getElementById("item-amount").value
-    let expenseDate = document.getElementById("expense-date").value
+    let itemName = document.getElementById("item-name").value.trim()
+    let itemCategory = document.getElementById("category").value.trim()
+    let itemAmount = document.getElementById("item-amount").value.trim()
+    let expenseDate = document.getElementById("expense-date").value.trim()
     console.log(itemName)
 
-    if (itemName && itemCategory && itemAmount && expenseDate) {
+    let numberList = document.getElementsByClassName("nr")
+    for (let i = 0; i < numberList.length; i++) {
+        numberList[i].innerHTML = (i + 1) + "."
 
-        itemList.innerHTML =
-        `<tr class="*:p-2 *:font-sans">
+    }
+    // console.log(numberList)
 
-           <td>${itemName}</td>
-           <td>${itemCategory}</td> 
-           <td>${itemAmount}</td>
-           <td>${expenseDate}</td>
 
-        </tr>
-        `
+    if (itemName == "" || !itemCategory || !itemAmount || !expenseDate) {
+        document.querySelector("#item-name").style.borderColor = "red"
 
+
+
+    } else {
+        const itemList = document.querySelector("#item-list")
+        const row = document.createElement("tr")
+        row.innerHTML =
+            `  <td class="nr"></td>
+               <td>${itemName}</td>
+               <td>${itemAmount}</td>
+               <td>${expenseDate}</td>
+               <td>${itemCategory}</td> 
+               <td class="">
+                  <a class="border bg-gray-300 text-sm" id="edit-btn">Edit</a>
+                  <a class="border bg-red-500 text-sm" id="delete-btn">Delete</a>
+               </td>
+             `
+        itemList.appendChild(row)
         closeModal()
 
-    }else{
-        
+
     }
+
+    clearInput()
 }
 
+function clearInput() {
+    document.getElementById("item-name").value = ""
+    document.getElementById("category").value = ""
+    document.getElementById("item-amount").value = ""
+    document.getElementById("expense-date").value = ""
+}
 
 
 
