18714be7ce9fa56a5d70904c7b99f38c739d9781
brymmobaggins <ibro2tablet21@yahoo.com>
2024-05-26 11:27:59 +0100

    add create custom animation with keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
-    fontFamily: {
-    }
+    extend: {
+      keyframes: {
+        heartbeat: {
+          '0%': { transform: 'rotate(0deg) scale(0.9)' },
+          // '25%': { transform: 'rotate(0deg) scale(1)' },
+          // '30%': { transform: 'rotate(0deg) scale(1.4)' },
+          '35%': { transform: 'rotate(0deg) scale(1.1)' },
+          // '70%': { transform: 'rotate(0deg) scale(1.4)' },
+          // '100%': { transform: 'rotate(0deg) scale(1)' },
+        },
+      },
+      animation: {
+        heartbeat: 'heartbeat 0.5s linear',
+      },
+    },
+  },
+  fontFamily: {
   },
   plugins: [],
 }
-
