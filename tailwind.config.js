8b7b3d8d7c3be40b08db7a5ac9fc435d0bc2a1e4
brymmobaggins <ibro2tablet21@yahoo.com>
2024-07-21 22:01:02 +0100

    create custom screen configuation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,42 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./dist/**/*.{hbs,js}",
-    "./views/**/*.{hbs,js}",
-    "./controllers/**/*.{hbs,js}",
-    "./routes/**/*.{hbs,js}"],
-  theme: {
-    screen: {
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
 
-      '2xl': '1536px',
-      '3xl': '1792px',
-      '4xl': '2048px',
-    },
-    fontFamily: {
-      "barrio": ["barrio", 'system-ui']
-    },
-    extend: {
-      keyframes: {
-        heartbeat: {
-          '0%': { transform: 'rotate(0deg) scale(0.9)' },
-          // '25%': { transform: 'rotate(0deg) scale(1)' },
-          // '30%': { transform: 'rotate(0deg) scale(1.4)' },
-          '35%': { transform: 'rotate(0deg) scale(1.1)' },
-          // '70%': { transform: 'rotate(0deg) scale(1.4)' },
-          // '100%': { transform: 'rotate(0deg) scale(1)' },
-        },
+export const content = [
+  "./dist/**/*.{hbs,html,js}",
+  "./views/**/*.{hbs,html,js}",
+  "./controllers/**/*.{hbs,html,js}",
+  "./routes/**/*.{hbs,html,js}",
+  ".",
+];
+export const theme = {
+  screens: {
+    // => @media (min-width: 425px) { ... }
+    mobile: "425px",
+    // => @media (min-width: 768px) { ... }
+    tablet: "768px",
+    // => @media (min-width: 1024px) { ... }
+    laptop: "1024px",
+
+  },
+  fontFamily: {
+    barrio: ["barrio", "system-ui"],
+  },
+  extend: {
+    keyframes: {
+      heartbeat: {
+        "0%": { transform: "rotate(0deg) scale(0.9)" },
+        // '25%': { transform: 'rotate(0deg) scale(1)' },
+        // '30%': { transform: 'rotate(0deg) scale(1.4)' },
+        "35%": { transform: "rotate(0deg) scale(1.1)" },
+        // '70%': { transform: 'rotate(0deg) scale(1.4)' },
+        // '100%': { transform: 'rotate(0deg) scale(1)' },
       },
-      animation: {
-        heartbeat: 'heartbeat 0.2s linear',
-      },
+    },
+    animation: {
+      heartbeat: "heartbeat 0.2s linear",
     },
   },
-
-  plugins: [],
-}
+};
+export const plugins = [];
